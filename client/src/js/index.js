b4a2685bc72c7e0dd71b68e5b197581807c8eb35
thiago <nasciutti.thiago@gmail.com>
2022-11-08 19:59:44 -0500

    indexeddb

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,17 +1,27 @@
 import "./form";
 import "./submit";
+
+
+
+//Import Bootstrap
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+//Import images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+//On load functionality
 window.addEventListener('load', function () {
-    document.getElementById('logo').src = Logo;
-    document.getElementById('bearThumbnail').src = Bear;
-    document.getElementById('dogThumbnail').src = Dog;
-  });
+  initdb();
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
+});
 
-    // Import CSS files
-    import "../css/index.css";
+// Import CSS files
+import "../css/index.css";
+
+//Import IndexedDB database
+import { initdb } from './database';
