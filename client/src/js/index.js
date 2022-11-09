1999fe338e06e1e47adb9481f53f984f5d6b76a1
thiago <nasciutti.thiago@gmail.com>
2022-11-09 10:14:05 -0500

    indexeddb

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,5 @@
 import "./form";
-import "./submit";
+
 
 
 
@@ -7,21 +7,65 @@ import "./submit";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Import CSS files
+import "../css/index.css";
+
 //Import images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+
+import { fetchCards } from './cards';
+import { toggleForm, clearForm } from './form';
+
+//Import IndexedDB database and CRUD Functions
+import { initdb, getDb, postDb } from './database';
+
 //On load functionality
 window.addEventListener('load', function () {
   initdb();
+  fetchCards();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
 });
 
-// Import CSS files
-import "../css/index.css";
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
 
-//Import IndexedDB database
-import { initdb } from './database';
+newContactButton.addEventListener('click', event => {
+  toggleForm()
+})
+
+form.addEventListener('submit', event => {
+  // Handle data
+  event.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+  if (submitBtnToUpdate == false) {
+    postDb(name, email, phone, profile);
+  } else {
+
+    fetchCards();
+    // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
+});
+
+
+
